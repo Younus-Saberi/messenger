cb2b4034e0334b7ecc5d11cb8ae3fafb3b48d16e
Younus-Saberi <younussaberi@gmail.com>
2025-04-30 08:29:39 +0000

    Add Logic for WebSocket(Pub/Sub)

---
+++ src/main/java/com/example/messenger/Greeting.java
@@ -0,0 +1,14 @@
+package com.example.messenger;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@NoArgsConstructor
+public class Greeting {
+    private String content;
+}
